c18ff682203a8659df9829be51e1015aaafd957e
WaKaN <wakan_powa_s01@hotmail.fr>
2017-05-28 19:48:14 +0200

    touchpad moins vite

--- 510-touchpad.nix
+++ 510-touchpad.nix
@@ -4,7 +4,7 @@
       services.xserver.synaptics = {
         enable = true;
         twoFingerScroll = true;
-        maxSpeed = "2.0";
+        maxSpeed = "1.0";
 	minSpeed = "0.5";
 	accelFactor = "0.25";
 };
