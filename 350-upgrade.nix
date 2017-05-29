e9b880f13e8dfd54c76ce33d15685a78ca68338c
WaKaN <wakan_powa_s01@hotmail.fr>
2017-05-29 07:47:35 +0200

    nix job en parallele

--- 350-upgrade.nix
+++ 350-upgrade.nix
@@ -4,7 +4,8 @@
   system.autoUpgrade.enable = true;
   system.autoUpgrade.channel = https://nixos.org/channels/nixos-17.03;
 
-
+  nix.maxJobs = 0; #0 calcul la bonne valeur en fonction du nombre de coeur de la machine
+  
   # The NixOS release to be compatible with for stateful data such as databases.
   system.stateVersion = "17.03";
 }
