742dbf0a783b27becfa3fe4be26907337a598769
WaKaN <wakan_powa_s01@hotmail.fr>
2018-01-06 17:50:59 +0100

    bon fichier de config

--- 552-i3.nix
+++ 552-i3.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   services.xserver.windowManager.i3.enable = true;
-  services.xserver.windowManager.i3.configFile = "./config/i3.config";
+  services.xserver.windowManager.i3.configFile = ./conf/i3.conf;
   environment.systemPackages = with pkgs ; [
     dmenu
   ];
