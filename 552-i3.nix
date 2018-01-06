10445f935d5f8c6e823d2dba5f4570e8c43342c6
WaKaN <wakan_powa_s01@hotmail.fr>
2018-01-06 17:35:43 +0100

    Passage sur i3 avec conf

---
+++ 552-i3.nix
@@ -0,0 +1,9 @@
+{ pkgs, ... }:
+{
+  services.xserver.windowManager.i3.enable = true;
+  services.xserver.windowManager.i3.configFile = "./config/i3.config";
+  environment.systemPackages = with pkgs ; [
+    dmenu
+  ];
+}
+
---
+++ 600-app_graphics.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }:
+{
+
+  programs.chromium.enable = true;
+  programs.gphoto2.enable = true;
+
+  environment.systemPackages = with pkgs ; [
+    rxvt_unicode
+    xcompmgr
+    zathura
+    libreoffice
+    skype
+    vlc
+    smplayer
+    gimp
+    geeqie
+    firefox
+    glmark2
+    atom
+    (import ./610-my-hello.nix)
+  ];
+}
