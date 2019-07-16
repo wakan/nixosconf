7d0d80f8106b60f0ec72c4ebaa7d95d9a6cc231b
WaKaN <wakan_powa_s01@hotmail.fr>
2019-07-16 09:07:18 +0200

    suppression du fichier de conf

--- 552-i3.nix
+++ 552-i3.nix
@@ -1,7 +1,6 @@
 { pkgs, ... }:
 {
   services.xserver.windowManager.i3.enable = true;
-  services.xserver.windowManager.i3.configFile = /home/guest/git/nixosconf/conf/i3.conf;
   environment.systemPackages = with pkgs ; [ 
     dmenu 
     i3status 
