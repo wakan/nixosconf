fc83df17b80da719a319c06e9df8d00283b82620
WaKaN <wakan_powa_s01@hotmail.fr>
2020-04-19 01:07:52 +0200

    bump nixos version

--- 350-upgrade.nix
+++ 350-upgrade.nix
@@ -2,5 +2,5 @@
 
 {
   system.autoUpgrade.enable = true;
-  system.autoUpgrade.channel = https://nixos.org/channels/nixos-19.09;
+  system.autoUpgrade.channel = https://nixos.org/channels/nixos-20.03;
 }
