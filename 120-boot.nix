e33fe10c175081683b63ba5b9493500064fca669
WaKaN <wakan_powa_s01@hotmail.fr>
2024-05-19 18:37:17 +0200

    virtualbox is not uefi faire ca mieux

--- 120-boot.nix
+++ 120-boot.nix
@@ -1,5 +1,5 @@
 { ... }:
 {
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.configurationLimit = 42;
 }
