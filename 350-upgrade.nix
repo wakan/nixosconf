2a9e42c05dcbef47246e73a4cf19d03df09215c6
WaKaN <wakan_powa_s01@hotmail.fr>
2024-05-19 16:43:18 +0200

    current configuration in run

--- 350-upgrade.nix
+++ 350-upgrade.nix
@@ -3,4 +3,6 @@
 {
   system.autoUpgrade.enable = true;
   system.autoUpgrade.channel = https://nixos.org/channels/nixos-20.03;
+  #/run/current-system/configuration.nix
+  system.copySystemConfiguration = true;
 }
