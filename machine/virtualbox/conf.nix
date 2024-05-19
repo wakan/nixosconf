ca13a0bd5b7c744d6eb91fa06a1989e9e53e4e4e
WaKaN <wakan_powa_s01@hotmail.fr>
2024-05-19 17:36:01 +0200

    add upgrade file in virtualbox machine

--- machine/virtualbox/conf.nix
+++ machine/virtualbox/conf.nix
@@ -8,6 +8,7 @@
       ../../150-firewall.nix
       ../../200-geographie.nix
       ../../300-users.nix
+      ../../350-upgrade.nix
       ../../400-services.nix
       ../../450-app_console.nix
       ../../500-xserver.nix
