0d3c4546d88ca6b29617d2bb2cff380d5852cfbc
WaKaN <wakan_powa_s01@hotmail.fr>
2024-05-19 21:55:50 +0200

    add user

--- 300-users.nix
+++ 300-users.nix
@@ -1,19 +1,25 @@
 { ... }:
 
 {
-  users.users = {
-    guest = {
-        name = "m";
-        extraGroups =
-          [
-            "wheel" "disk" "audio" "video"
-	          "camera"
-            "networkmanager" "systemd-journal"
-          ];
-        isNormalUser = true;
-        uid = 1000;
-	openssh.authorizedKeys.keys  =
-	    with import ./misc/ssh-keys.nix; [ guestportmsi ];
-    };
+  users.users = 
+  let commonUserConfig =
+  {
+      extraGroups =
+      [
+        "wheel" "disk" "audio" "video"
+        "camera"
+        "networkmanager" "systemd-journal"
+      ];
+      isNormalUser = true;
+      openssh.authorizedKeys.keys  =
+        with import ./misc/ssh-keys.nix; [ guestportmsi ];
+  }; in
+  {
+    guest = (commonUserConfig // { name = "guest"; });
+    perso = (commonUserConfig // { name = "perso"; });
+    achat = (commonUserConfig // { name = "achat"; });
+    hopps = (commonUserConfig // { name = "hopps"; });
+    bank = (commonUserConfig // { name = "bank"; });
+    dev = (commonUserConfig // { name = "dev"; });
   };
 }
