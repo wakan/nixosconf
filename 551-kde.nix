1fa45e41f299006d2fb3de2a8dab2b6d2b29e07c
WaKaN <wakan_powa_s01@hotmail.fr>
2024-05-19 16:47:01 +0200

    fixed kde syntax

--- 551-kde.nix
+++ 551-kde.nix
@@ -1,6 +1,10 @@
 { ... }:
 
 {
-  sddm.enable = true;
-  desktopManager = { plasma5.enable = true; };
+  services.xserver = {  
+    displayManager = {
+      sddm.enable = true;
+    };
+    desktopManager = { plasma5.enable = true; };
+  };
 }
