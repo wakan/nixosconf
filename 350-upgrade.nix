3f9993f3d2ce6ce565b36a67fef0850cd2198f99
WaKaN <wakan_powa_s01@hotmail.fr>
2024-05-20 19:09:13 +0200

    upgrade in correct version

--- 350-upgrade.nix
+++ 350-upgrade.nix
@@ -2,9 +2,11 @@
 
 {
   system.autoUpgrade.enable = true;
-  system.autoUpgrade.channel = https://nixos.org/channels/nixos-20.03;
+  system.autoUpgrade.channel = https://nixos.org/channels/nixos-23.11;
   #/run/current-system/configuration.nix
   system.copySystemConfiguration = true;
+
+  system.stateVersion = 23.11;
   
   nix.settings.auto-optimise-store = true;
   nix.gc = {
